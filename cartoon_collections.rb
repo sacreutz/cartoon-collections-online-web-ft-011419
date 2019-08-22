5e67eae9afc70561fd05e684400427b788c12187
Sophie Creutz <sophie.creutz@flatironschool.com>
2019-08-22 23:46:05 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,10 +14,9 @@ def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   cheese_types.each do |cheese|
-    if array.include?(cheese_types)
-  if result
-    return result
-  else 
-    nil 
+    if array.include?(cheese)
+      return cheese 
+    else 
+      return nil
   end 
 end
